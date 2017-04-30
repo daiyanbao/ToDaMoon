68e2c350f5c1204b59e09d19b0df263c042847bc
aQua <aQuaYi@users.noreply.github.com>
2017-04-30 15:27:16 +0800

    添加了版本号功能

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ToDaMoon
-
+ToDaMoon是我的自己的虚拟币交易系统。
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var (
+	//Version 版本号
+	Version string
+	//BuildTime 编译时间
+	BuildTime string
+	//GitHash 当前的Git Hash码
+	GitHash string
+	//BuildNumber 编译次数
+	BuildNumber string
+)
+
+func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "version" || os.Args[1] == "v") {
+		fmt.Println("Version: ", Version+"."+BuildNumber)
+		fmt.Println("Time:    ", BuildTime)
+		fmt.Println("GitHash: ", GitHash)
+		return
+	}
+}
