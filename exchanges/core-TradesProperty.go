837c807d03bd0e198bc619c69765915400054740
aQua <aQuaYi@users.noreply.github.com>
2017-06-05 17:48:11 +0800

    想到了一个实现修改更新周期的方法。

--- exchanges/core-TradesProperty.go
+++ exchanges/core-TradesProperty.go
@@ -9,17 +9,25 @@ import (
 	"github.com/imkira/go-observer"
 )
 
-//TradesProperty 是exchange的trades监听属性
-type TradesProperty map[string]map[string]observer.Property
+//TradeSubject 会发布最新的交易数据。
+//会通过observer.Property来更新最新的交易数据。
+//updateCycleCh 可以修改Property的更新周期
+type TradeSubject struct {
+	observer.Property
+	UpdateCycleCh chan time.Duration
+}
 
-func makeProperties(e Exchanger, tdbs TradesDBs, sleepTime time.Duration) TradesProperty {
+//TradesProperty 是exchnge的trades监听属性
+type TradesProperty map[string]map[string]TradeSubject
+
+func makeProperties(e Exchanger, tdbs TradesDBs, defaultUpdateCycle time.Duration) TradesProperty {
 	tp := make(TradesProperty)
 	wg := &sync.WaitGroup{}
 	for money, coinDBs := range tdbs {
-		tp[money] = make(map[string]observer.Property)
+		tp[money] = make(map[string]TradeSubject)
 		for coin, db := range coinDBs {
 			wg.Add(1)
-			tp[money][coin] = makePropertyAndSaveToDB(e, money, coin, db, sleepTime, wg)
+			tp[money][coin] = makePropertyAndSaveToDB(e, money, coin, db, defaultUpdateCycle, wg)
 		}
 	}
 
@@ -30,20 +38,25 @@ func makeProperties(e Exchanger, tdbs TradesDBs, sleepTime time.Duration) Trades
 	return tp
 }
 
-func makePropertyAndSaveToDB(e Exchanger, money, coin string, db *TradesDB, sleepTime time.Duration, wg *sync.WaitGroup) observer.Property {
+func makePropertyAndSaveToDB(e Exchanger, money, coin string, db *TradesDB, defaultUpdateCycle time.Duration, wg *sync.WaitGroup) TradeSubject {
 	th := Trades{}
 	p := observer.NewProperty(th)
-	go updatePropertyAndSaveToDB(e, money, coin, p, db, sleepTime)
+	ch := updatePropertyAndSaveToDB(e, money, coin, p, db, defaultUpdateCycle)
 	wg.Done()
-	return p
+	return TradeSubject{
+		Property:      p,
+		UpdateCycleCh: ch,
+	}
 }
 
-func updatePropertyAndSaveToDB(e Exchanger, money, coin string, p observer.Property, db *TradesDB, sleepTime time.Duration) {
+func updatePropertyAndSaveToDB(e Exchanger, money, coin string, p observer.Property, db *TradesDB, defaultUpdateCycle time.Duration) chan time.Duration {
 	maxTid, err := db.MaxTid()
 	if err != nil {
 		msg := fmt.Sprintf("updatePropertyAndSaveToDB(): 无法获取%s数据库的MaxTid: %s", db.Name(), err)
 		log.Fatalln(msg)
 	}
+	ch := make(chan time.Duration, 1)
+	updateCycle := defaultUpdateCycle
 
 	for {
 		th, err := e.Trades(money, coin, maxTid)
@@ -63,6 +76,14 @@ func updatePropertyAndSaveToDB(e Exchanger, money, coin string, p observer.Prope
 			}
 		}
 		//REVIEW: 无法动态调整sleep的时间，是一个非常大的问题。特别是当exchange中的coin的种类特别多的时候。
-		time.Sleep(sleepTime)
+		//FIXME:  把这个重构成一个独立的函数。
+		select {
+		case uc := <-ch:
+			updateCycle = uc
+		default:
+			updateCycle = defaultUpdateCycle
+		}
+		time.Sleep(time.Second)
+
 	}
 }
