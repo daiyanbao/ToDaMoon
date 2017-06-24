c50f8206291c293e0b89cd13a79917918dae3247
aQua <aQuaYi@users.noreply.github.com>
2017-06-24 20:02:02 +0800

    修改了判断两个变量的类型是否相等的方法。

--- util/struct.go
+++ util/struct.go
@@ -1,13 +1,13 @@
 package util
 
-import "fmt"
+import "reflect"
 
 //IsTypeEqual 判断两个interface{}类型的变量的原始类型是否相等
-//TODO: 找到一种更好的判断方法
 func IsTypeEqual(a, b interface{}) bool {
-	aType := fmt.Sprintf("%T", a)
-	bType := fmt.Sprintf("%T", b)
-	if aType == bType {
+	at := reflect.TypeOf(a).Kind()
+	bt := reflect.TypeOf(b).Kind()
+
+	if at == bt {
 		return true
 	}
 	return false
